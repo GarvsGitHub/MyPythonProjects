04d76c4098714d0729003d49fd1bb69991c1cfbd
GarvsGitHub <140343276+GarvsGitHub@users.noreply.github.com>
2023-08-11 03:16:34 +0530

    Update main.py

--- Day-16/main.py
+++ Day-16/main.py
@@ -1,4 +1,6 @@
-# Constructing Objects and Accessing their Attributes and Methods
+# ===== Constructing Objects and Accessing their Attributes and Methods ===== #
+
+# ===== Working on inbuilt turtle graphic package ===== #
 # from turtle import Turtle, Screen
 #
 # kurma = Turtle()  # Created object named kurma and assigned blueprint(class) to it
@@ -11,11 +13,13 @@
 # print(my_screen.canvheight)  # Used attribute of class i.e. my_screen.canvheight
 # my_screen.exitonclick()
 
+
+# ===== Working on downloaded package of prettytable ==== #
 from prettytable import PrettyTable
 table = PrettyTable()
-table.add_column("Pokemon Name", ["Pikachu", "Squirtle", "Charmander"])
+table.add_column("Pokemon Name", ["Pikachu", "Squirtle", "Charmander"])  # .add_column("Fieldname_of_Table", ["Attributes in the field"]
 table.add_column("Type", ["Electric", "Water", "Fire"])
-# table.align = "l"
-table.align["Pokemon Name"] = "l"
+# table.align = "l"  # For entire table(l=left)
+table.align["Pokemon Name"] = "l"  # For a specific field(r=right) 
 table.align["Type"] = "r"
 print(table)
