96d25b1e7124cb54c8f0b7d21d68dd7b05f06c2c
GarvsGitHub <140343276+GarvsGitHub@users.noreply.github.com>
2023-08-11 18:22:44 +0530

    Update main.py

--- Day-1 Brand Name Generator/main.py
+++ Day-1 Brand Name Generator/main.py
@@ -1,9 +1,9 @@
 # 1. Create a greeting for your program.
 print("Welcome to the Band name generator.")
 # 2. Ask the user for the city that they grew up in.
-city_name = input("What's name of the city you grew up in?\n")
+city = input("What's name of the city you grew up in?\n")
 # 3. Ask the user for the name of a pet.
-pet_name = input("What's your pet's name?\n")
+pet = input("What's your pet's name?\n")
 # 4. Combine the name of their city and pet and show them their band name.
-print("Your band name is: "+city_name + " " + pet_name+"\n")
+print("Your band name could be " + city + " " + pet)
 # 5. Make sure the input cursor shows on a new line:
