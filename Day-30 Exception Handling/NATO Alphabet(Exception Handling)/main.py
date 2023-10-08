46af868e0fe3bdc0181518cda4bcb12f7e640f2b
Gaurav Dubey <140343276+GarvsGitHub@users.noreply.github.com>
2023-10-08 17:54:11 +0530

    Update main.py

--- Day-30 Exception Handling/NATO Alphabet(Exception Handling)/main.py
+++ Day-30 Exception Handling/NATO Alphabet(Exception Handling)/main.py
@@ -1,20 +1,12 @@
+# ===== Welcome back to the NATO Phonetic Dictionary ===== #
+# In this project we have added exception handling for wrong user input and have also used List Comprehension.
+# Other than this minor changes rest of the functionality is same as previous project of NATO Alphabet.
 import pandas
 
 data = pandas.read_csv("nato_phonetic_alphabet.csv")
 
 phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}  # Dictionary Comprehension
 print(phonetic_dict)
-
-# valid = True
-# while valid:
-#     try:
-#         word = input("Enter the word: ").upper()
-#         output_list = [phonetic_dict[letter] for letter in word]  # List Comprehension
-#     except KeyError:
-#         print("Sorry, only letters in the alphabet please.")
-#     else:
-#         print(output_list)
-#         valid = False
 
 
 def generate_phonetic():
