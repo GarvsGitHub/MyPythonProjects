49372cc9c69f9b88cddc4fcfbeade059f6925329
Gaurav Dubey <140343276+GarvsGitHub@users.noreply.github.com>
2024-03-05 15:11:57 +0530

    Update main.py

--- Day-37 Habit Tracking Project/main.py
+++ Day-37 Habit Tracking Project/main.py
@@ -1,13 +1,16 @@
+#====== Welcome to the Habit Tracking project ======
+# This project mainly focuses on HTTP requests like POST(Create), PUT(Update), Delete.
+# Here we are using pixela website to track our habit by daily uploading data on it.
+
 import requests
 from datetime import datetime
 
-USERNAME = "garvswork"
-TOKEN = "Gd@pixela88"
+USERNAME = "Your Username"
+TOKEN = "Your Pwd/Token"
 
-# How to create a user on Pixela?
+#====> How to create a user on Pixela?
 pixela_endpoint = "https://pixe.la/v1/users"
 
-# parameters to create a user
 user_params = {
     "token": TOKEN,
     "username": USERNAME,
@@ -15,11 +18,10 @@ user_params = {
     "notMinor": "yes"
 }
 
-# post method to create a user
 # response = requests.post(url=pixela_endpoint, json=user_params)
 # print(response.text)
 
-# How to create a graph on pixela?
+#====> How to create a graph on pixela?
 graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"  # url
 
 graph_config = {
@@ -37,20 +39,20 @@ headers = {
 # response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
 # print(response.text)
 
-# How to post a pixel on pixela?
+#====> How to create a pixel on graph?
 pixel_post_endpoint = f"{graph_endpoint}/graph1"
 
 today = datetime.now()
 
 pixel_config = {
-    "date": today.strftime("%Y%m%d"),  # Here we wanted date in yyyymmdd format which is mandatory on pixel site
+    "date": today.strftime("%Y%m%d"),  # Here we wanted date in yyyymmdd format which was mandatory according to the site
     "quantity": "4",
 }
 
 # response = requests.post(url=pixel_post_endpoint, json=pixel_config, headers=headers)
 # print(response.text)
 
-# How to Update pixela graph?
+#====> How to Update pixela graph?
 Update_date = datetime(year=2024, month=2, day=29)
 update_date_format = Update_date.strftime("%Y%m%d")
 
@@ -63,7 +65,7 @@ update_param = {
 # response = requests.put(url=update_endpoint, json=update_param, headers=headers)
 # print(response.text)
 
-# How to delete a pixel?
+#====> How to delete a pixel?
 delete_date = update_date_format
 delete_endpoint = f"{pixel_post_endpoint}/{delete_date}"
 
